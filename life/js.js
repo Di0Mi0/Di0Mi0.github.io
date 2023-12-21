b7d892b4ca3a8b326395f059e7ee22b90822e936
Di0Mi0 <144342889+Di0Mi0@users.noreply.github.com>
2023-12-21 10:27:07 +0300

    Update js.js

--- life/js.js
+++ life/js.js
@@ -37,7 +37,7 @@ goLife();
 function drawField(){
 	ctx.clearRect(0, 0, 600, 600);
 	for (var i=0; i<60; i++){
-		for (var j=0; j<30; j++){
+		for (var j=0; j<60; j++){
 			if (mas[i][j]==1){
 				ctx.fillRect(j*10, i*10, 10, 10);
 			}
